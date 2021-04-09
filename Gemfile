c8db222ebe79f9e5ae1f9d91c8714aa632a0c829
kiyomasa05 <m230549@gmail.com>
2021-04-10 08:48:15 +0900

    heroku デプロイ

--- Gemfile
+++ Gemfile
@@ -36,5 +36,9 @@ group :development do
   gem 'spring'
 end
 
+group :production do # 本番環境
+  #gem 'pg' # PostgreSQL
+end
+
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.assets.compile = true
+config.assets.initialize_on_precompile=false
 end
