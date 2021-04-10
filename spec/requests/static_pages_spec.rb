6bc0c2de03cb44a7773fab4c4ca8af40db86d91d
kiyomasa05 <m230549@gmail.com>
2021-04-10 10:49:09 +0900

    reactインストール

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "静的ページ", type: :request do
+  describe "トップページ" do
+    it "正常なレスポンスを返すこと" do
+      get root_path
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+  end
+end
