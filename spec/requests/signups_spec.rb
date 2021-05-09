906a34888764ad3df357e0ef2d9d26f15b3685c0
kiyomasa05 <m230549@gmail.com>
2021-05-09 09:14:46 +0900

    ログイン周りrspec記述OK

--- spec/requests/signups_spec.rb
+++ spec/requests/signups_spec.rb
@@ -2,15 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "Signups", type: :request do
   describe "post/signups" do
-    
-      it '新しいUserを作成する' do
-        post 'http://localhost:3000/api/v1/signup',
-        params:{user:{name: 'aaaaa', email: 'sample@sample.com', password: '1234', password_confirmation: '1234' }}
-
-        expect(response).to have_http_status(:ok)
-        #ポストデータを受け取り、Userのカウントを1増やす
-        # expect().to change(User, :count).by(+1)
-        
+    signup_url='http://localhost:3000/api/v1/signup'
+    context '正常可動' do
+      
+        it '新しいUserを作成する' do
+          expect do
+            post signup_url,params:{user:{name:"sample",email:"sample@sample.com",password:"foobar",password_confirmation:"foobar"}}
+            expect(response).to have_http_status(:ok)
+            #ポストデータを受け取り、Userのカウントを1増やす
+          end.to change {User.count}.by(+1)
+        end
+        it '新しいUserを作成後ログインする' do
+          post signup_url,params:{user:{name:"sample",email:"sample@sample.com",password:"foobar",password_confirmation:"foobar"}}
+          expect(is_logged_in?).to be_truthy
+        end
+        it '新しいUserを作成後jsonデータを返す' do
+          post signup_url,params:{user:{name:"sample",email:"sample@sample.com",password:"foobar",password_confirmation:"foobar"}}
+          expect(json['status']).to eq("created")
+          expect(json['logged_in']).to eq(true)
+          expect(json["user"]["name"]).to eq("sample")
+          expect(json["user"]["email"]).to eq("sample@sample.com")
+        end
+    end
+    context 'エラー時' do
+      it 'ステータスコード500をjsonで返す' do
+        post signup_url,params:{user:{name:"sample",email:"",password:"foobar",password_confirmation:"foobar"}}
+        expect(json['status']).to eq(500)
+      end
     end
   end
 end
