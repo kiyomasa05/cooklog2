f8d60a11f2821c91d6c0c2709f26d701b2e4a951
kiyomasa05 <m230549@gmail.com>
2021-05-12 11:24:43 +0900

    フロント投稿ページ2

--- frontend/src/containers/Post.jsx
+++ frontend/src/containers/Post.jsx
@@ -54,32 +54,36 @@ export default function Post(props) {
     axios.post(post,
       {
         recipe: {
-          name: data.name,
-          email: data.email,
-          password: data.password,
-          password_confirmation: data.password_confirmation,
+          user_id:props.user.id,
+          title: data.title,
+          time_required: data.time_required,
+          food: data.food,
+          process: data.process,
         }
-      },
-      { withCredentials: true }
+      }
     ).then(response => {
       console.log(response)
     }).catch(error => {
       console.log("registration error", error)
     })
   }
-
+  // ここにログインユーザーのidをセットでpostする必要がある
+  // まずはpostできるようにする　デザインは後　写真のアップロードも必要
+  //バックエンドでルーティングとアクションも必要
 
   return (
     <>
       <Header />
       <Title>post</Title>
+      <h2>ログイン状態: {props.loggedInStatus}</h2>
+      <h2>ユーザー: {props.user.name}さん</h2>
 
       <form onSubmit={handleSubmit(onSubmit)}>
         {/* 要バリデート作成＆反映 */}
         <Input type="text" placeholder="タイトル" {...register("title", { required: true, maxLength: 80 })} />
         {errors.title && <p>"正しく入力してください"</p>}
 
-        <Input type="text" placeholder="所要時間" {...register("time_required", { required: true, })} />
+        <Input type="time" placeholder="所要時間" {...register("time_required", { required: true, })} />
         {errors.time_required && <p>"正しく入力してください"</p>}
 
         <Input type="text" placeholder="材料" {...register("food", { required: true, minLength: 4 })} />
@@ -87,7 +91,7 @@ export default function Post(props) {
 
         <Input type="text" placeholder="手順" {...register("process", { required: true, })} />
         {errors.process && <p>"正しく入力してください"</p>}
-        <Submit type="submit" value="新規登録" />
+        <Submit type="submit" value="レシピ登録" />
       </form>
     </>
   );
