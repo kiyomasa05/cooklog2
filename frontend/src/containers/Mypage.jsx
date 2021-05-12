26fe73956df15b66dc4e36551384bc156448f423
kiyomasa05 <m230549@gmail.com>
2021-05-12 10:29:35 +0900

    マイページにpostへのリンク追加

--- frontend/src/containers/Mypage.jsx
+++ frontend/src/containers/Mypage.jsx
@@ -1,6 +1,7 @@
-import React ,{ Component }from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
 import '../App.css';
+import { Link } from "react-router-dom";
 
 //apis
 // import { fetchMypage }from '../apis/mypage'
@@ -11,15 +12,18 @@ import { Container } from '../component/wrapper/Login_Wrapper'
 
 export default function Mypage(props) {
   // app.jsからprops(ログイン状態)を受け取るための引数
-  
-    return (
-      <div>
-        <Header />
-        <Container>
-          マイページ
+
+  return (
+    <div>
+      <Header />
+      <Container>
+        マイページ
           <h2>ログイン状態: {props.loggedInStatus}</h2>
-          <h2>ユーザー: {props.user.name}さん</h2>
-        </Container>
-      </div>
-    );
+        <h2>ユーザー: {props.user.name}さん</h2>
+        <Link to="/post">
+          料理投稿
+          </Link>
+      </Container>
+    </div>
+  );
 }
