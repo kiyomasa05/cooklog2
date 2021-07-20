198ade225ff83d1356aa4d04735e168aba362727
kiyomasa05 <m230549@gmail.com>
2021-07-21 08:41:56 +0900

    画像投稿機能完了（一部画像によって送信できないエラーあり）

--- frontend/src/containers/Post.jsx
+++ frontend/src/containers/Post.jsx
@@ -85,11 +85,11 @@ export const Post = () => {
       }
       , { withCredentials: true }
     ).then(response => {
-      if (response.data.created) {
+      if (response.data.status = "created") {
         showMessage({ title: "投稿に成功しました", status: "success" });
         history.push("/index");
       }
-      else if (response.data.status === 500) {
+      else if (response.data.status === 422) {
         showMessage({ title: `${response.data.errors}`, status: "error" });
       }
     }).catch(e => {
@@ -102,7 +102,7 @@ export const Post = () => {
   return (
     <>
       <Flex mt="80px" alignItems="center" justifyContent="center" >
-        <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+        <Box bg="white" w={{ base: "sm", md: "2xl" }} p={4} borderRadius="md" shadow="md">
           <Heading as="h1" size="lg" textAlign="center">
             レシピ投稿
         </Heading>
@@ -110,24 +110,25 @@ export const Post = () => {
           <FormControl>
             <Stack>
               <FormControl>
-                <Input variant="flushed" radii="1rem" placeholder="タイトル(20文字まで)" value={title} onChange={e => setTitle(e.target.value)} />
+                <Input variant="flushed" fontSize={{ base: "md", md: "xl" }} radii="1rem" placeholder="タイトル(20文字まで)" value={title} onChange={e => setTitle(e.target.value)} />
               </FormControl>
             </Stack>
             <Stack>
-              <FormLabel color="gray.500" htmlFor="image" mt="4" mb="-2" >レシピ写真</FormLabel>
-              <Image src={!image.data ? "gibbresh.png" : image.data} fallbackSrc="https://via.placeholder.com/150" />
+              <FormLabel color="gray.500" htmlFor="image" mt="4" mb="-2" fontSize={{ base: "sm", md: "md" }}>レシピ写真</FormLabel>
+              <Image src={!image.data ? "gibbresh.png" : image.data} fallbackSrc="https://via.placeholder.com/250" boxSize={{ base: "250px", md: "400px" }} textAlign="center"
+              />
             </Stack>
             <Stack>
               <Input type="file" placeholder="画像アップロード" name="image" accept="image/png,image/jpeg" onChange={handleImageSelect} />
             </Stack>
 
             <Stack mt={3}>
-              <FormLabel color="gray.500" htmlFor="food" mb="-2">材料</FormLabel>
-              <Textarea id="food" placeholder="材料" value={food} onChange={e => setFood(e.target.value)} />
+              <FormLabel color="gray.500" htmlFor="food" mb="-2" fontSize={{ base: "sm", md: "md" }}>材料</FormLabel>
+              <Textarea id="food" placeholder="例）鶏肉、キャベツ、砂糖、塩..." value={food} fontSize={{ base: "sm", md: "md" }} onChange={e => setFood(e.target.value)} />
             </Stack>
             <Stack mt={3}>
-              <FormLabel color="gray.500" htmlFor="process" mb="-2">手順</FormLabel>
-              <Textarea id="process" placeholder="手順" value={process} onChange={e => setProcess(e.target.value)} />
+              <FormLabel color="gray.500" htmlFor="process" mb="-2" fontSize={{ base: "sm", md: "md" }}>手順</FormLabel>
+              <Textarea id="process" placeholder="例）1 キャベツを千切りしておく 2 鶏肉を茹でる..." value={process} fontSize={{ base: "sm", md: "md" }} onChange={e => setProcess(e.target.value)} />
             </Stack>
             <FormLabel color="gray.500" htmlFor="time_required" mt="2" mb="0">所要時間</FormLabel>
             <Flex>
