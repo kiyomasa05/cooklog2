c6d0667c8ec1138b77673158d802a9488f69705f
kiyomasa05 <m230549@gmail.com>
2021-12-27 13:50:50 +0900

    Dockerfileを元に戻し、corsの変更

--- api/config/initializers/cors.rb
+++ api/config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   # ローカル環境
   allow do
-    origins "*"
+    origins Rails.application.config.x.cors_allowed_origins
 
     resource "*",
       headers: :any,
