735275475fa418cfc05c6373db60085aef4f0b98
kiyomasa05 <m230549@gmail.com>
2022-01-01 12:01:44 +0900

    サブディレクトリを追加後、元のディレクトリ削除

--- api_cp/db/migrate/20210514003224_removeimage_from.rb
+++
@@ -1,8 +0,0 @@
-class RemoveimageFrom < ActiveRecord::Migration[6.1]
-  
-
-  def change
-    remove_column :recipes, :image, :string
-  end
-
-end
--- api_cp/spec/factories/favorites.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :favorite do
-    user { nil }
-    recipe { nil }
-  end
-end
